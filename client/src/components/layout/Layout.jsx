d34f0bc9b8224c3c7335d195ebe86dd169d99def
mtalha1501 <mtalha1501@gmail.com>
2026-08-11 17:42:58 +0500

    chore: hide footer from all internal application pages except landing and explore

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = () => {
+  const location = useLocation();
+  const showFooter = location.pathname === '/' || location.pathname === '/explore';
+
   return (
     <div className="min-h-screen flex flex-col bg-slate-950">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
+      {showFooter && <Footer />}
     </div>
   );
 };
