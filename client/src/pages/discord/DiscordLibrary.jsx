af18fcae6206f48df9519a5d8b272b69e0143810
mtalha1501 <mtalha1501@gmail.com>
2026-08-11 16:19:19 +0500

    fix: added applications.commands scope to Discord OAuth2 URL

--- client/src/pages/discord/DiscordLibrary.jsx
+++ client/src/pages/discord/DiscordLibrary.jsx
@@ -79,7 +79,7 @@ const DiscordLibrary = () => {
       if (!clientId) throw new Error("Missing client ID");
       
       const redirectUri = window.location.origin + '/discord-inbox';
-      const oauthUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=identify`;
+      const oauthUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=identify%20applications.commands`;
       
       window.location.href = oauthUrl;
     } catch (error) {
